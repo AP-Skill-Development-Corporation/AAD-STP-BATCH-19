054a5748d20207b74fb11e898f8c4f0b03070aaf
krishna <svkrishna.p@apssdc.in>
2020-09-22 17:52:03 +0530

    added

--- Intents_b19/app/src/main/java/com/facebook/intents_b19/SecondActivity.java
+++ Intents_b19/app/src/main/java/com/facebook/intents_b19/SecondActivity.java
@@ -3,12 +3,19 @@ package com.facebook.intents_b19;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class SecondActivity extends AppCompatActivity {
+    TextView t1;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_second);
+        t1 = findViewById(R.id.text1);
+        Bundle bundle = getIntent().getExtras();
+        String data =bundle.getString("KEY");
+        t1.setText(""+data);
     }
 }
