2983c5b0e39006d706887a34a130d6aa26961894
krishna <svkrishna.p@apssdc.in>
2020-09-22 17:52:03 +0530

    added

--- Intents_b19/app/src/main/java/com/facebook/intents_b19/MainActivity.java
+++ Intents_b19/app/src/main/java/com/facebook/intents_b19/MainActivity.java
@@ -3,19 +3,48 @@ package com.facebook.intents_b19;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText et1,et2,et3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        et1 = findViewById(R.id.edit1);
+        et2 = findViewById(R.id.edit2);
+        et3 = findViewById(R.id.edit3);
     }
 
     public void navigate(View view) {
         Intent intent = new Intent(this,SecondActivity.class);
         startActivity(intent);
+    }
+
+    public void sendUrl(View view) {
+        String browser = et1.getText().toString();
+        Uri uri = Uri.parse("https://www."+browser);
+
+        Intent intent = new Intent(Intent.ACTION_VIEW,uri);
+        startActivity(intent);
+    }
+
+    public void dial(View view) {
+        int num= Integer.parseInt(et2.getText().toString());
+        Uri uri = Uri.parse("tel:"+num);
+        Intent intent = new Intent(Intent.ACTION_DIAL,uri);
+        startActivity(intent);
+    }
+
+    public void sendData(View view) {
+        String data = et3.getText().toString();
+        Intent intent = new Intent(this,SecondActivity.class);
+        intent.putExtra("KEY",data);
+        startActivity(intent)
+        ;
     }
 }
